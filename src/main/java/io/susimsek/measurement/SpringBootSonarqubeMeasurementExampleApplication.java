107f4d44d0adac604366d400fcbea271bcecdf62
Şuayb Şimşek <suayb.simsek@detaysoft.com>
2021-06-23 17:56:27 +0300

    init commit

---
+++ src/main/java/io/susimsek/measurement/SpringBootSonarqubeMeasurementExampleApplication.java
@@ -0,0 +1,13 @@
+package io.susimsek.measurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarqubeMeasurementExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSonarqubeMeasurementExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/susimsek/measurement/SpringBootSonarqubeMeasurementExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.susimsek.measurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSonarqubeMeasurementExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
